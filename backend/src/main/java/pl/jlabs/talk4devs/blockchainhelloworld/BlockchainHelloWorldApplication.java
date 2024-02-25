bb4b9bb8a3add31996b2ddb405e96028d1934745
Redpike <rsokulski@op.pl>
2024-02-25 13:57:32 +0100

    Add Spring Boot and Angular templates

---
+++ backend/src/main/java/pl/jlabs/talk4devs/blockchainhelloworld/BlockchainHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package pl.jlabs.talk4devs.blockchainhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainHelloWorldApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/jlabs/talk4devs/blockchainhelloworld/BlockchainHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jlabs.talk4devs.blockchainhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
