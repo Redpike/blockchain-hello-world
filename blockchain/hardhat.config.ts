f5e8f571c02b3057ee90b1a7fc879b3771dc9cec
Redpike <rsokulski@op.pl>
2024-02-26 18:26:33 +0100

    Specify version of blockchain and frontend libraries

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -5,7 +5,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     talk4devs: {
-      url: 'http://localhost:7545',
+      url: 'http://localhost:18545',
       accounts: ['0x91fd4e8a060cceff00ae5cde99d5b167179f724d9a424e24672e4200c7679c98']
     }
   },
